b9b0cd5da4f2e0392a63945236783b1fea1e2327
noetix <lpjhblpj@163.com>
2025-09-12 14:41:27 +0800

    第一个Vue 文件

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <FirstVue message ="hello world"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+// import HelloWorld from './components/HelloWorld.vue'
+import FirstVue from "@/test/FirstVue.vue";
 
 export default {
   name: 'App',
+  // components: {
+  //   HelloWorld
+  // }
   components: {
-    HelloWorld
+    FirstVue
   }
 }
 </script>
---
+++ src/test/FirstVue.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>{{message}}</h1>
+    <button @click="changeMessage">Click me</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'FirstVue',
+  data() {
+    return {
+      message: 'Hello World'
+    }
+  },
+  methods: {
+    changeMessage() {
+      this.message = '消息已改变'
+    }
+  }
+}
+</script>
+
+
+
+<style scoped>
+ h1 {
+  color: red;
+ }
+
+ button {
+  background-color: blue;
+  color: white;
+ }
+
+</style>
